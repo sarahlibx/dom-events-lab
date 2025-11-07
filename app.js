7996fd8fec1a4b636fc2442d755cd1026aac492d
Sarah Smith <sarahlib90@gmail.com>
2025-11-07 17:56:07 -0500

    Added some event listeners

---
+++ app.js
@@ -0,0 +1,37 @@
+// num = button number
+// operator = button operator
+// equals = button equals
+
+/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    console.log(event.target.innerText);
+    // Future logic to capture the button's value would go here...
+  });
+});
+
+calculator.addEventListener('click', (event) => {
+  // This log is for testing purposes to verify we're getting the correct value
+  // You have to click a button to see this log
+  console.log(event.target.innerText);
+
+  // Example
+  if (event.target.classList.contains('number')) {
+    // Do something with a number
+  }
+
+  // Example
+  if (event.target.innerText === '*') {
+    // Do something with this operator
+  }
+});
